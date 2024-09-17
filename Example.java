71ed1e76b9197bd6503154918191c19b55147ca2
Stian Steinbakken <stian.steinbakken@posten.no>
2024-09-17 17:18:05 +0200

    feat: finish up most of the examples in the notebook

---
+++ Example.java
@@ -0,0 +1,9 @@
+public class Example {
+    public static void main(String[] args) {
+        var incremented = new Example().increment(1);
+    }
+
+    private int increment(int x) {
+        return x + 1;
+    }
+}
